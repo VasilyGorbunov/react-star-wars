45c0a2384adb6cfdb56c83f852390cc806ee87f3
VasilyGorbunov <vasily.gorbunov@gmail.com>
2021-12-23 19:03:48 +0300

    #13. Роутинг. URL Parameters. Компонент "Информация по персонажам"

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -1,26 +1,27 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-import styles from './PeopleList.module.css';
+import styles from './PeopleList.module.css'
 
 const PeopleList = ({ people }) => {
   return (
-      <>
-        <ul className={styles.list__container}>
-          { people.map(({id, name, img}) => 
-            <li key={id} className={styles.list__item}>
-              <a href="#">
-                <img className={styles.person__photo} src={img} alt={name} />
-                <p>{name}</p>
-              </a>
-            </li>
-          ) }
-        </ul>
-      </>
+    <>
+      <ul className={styles.list__container}>
+        {people.map(({ id, name, img }) => (
+          <li key={id} className={styles.list__item}>
+            <Link to={`/people/${id}`}>
+              <img className={styles.person__photo} src={img} alt={name} />
+              <p>{name}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
 PeopleList.propTypes = {
-    people: PropTypes.array,
+  people: PropTypes.array,
 }
 
-export default PeopleList;
+export default PeopleList
